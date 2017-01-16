9edcab514d36e9f26f3450c7b5c8cff8dc68e258
Bryan BRANDT <bbrandt@e1r8p17.42.fr>
2017-01-16 18:02:52 +0100

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrandt <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/08 14:26:48 by bbrandt           #+#    #+#             */
+/*   Updated: 2017/01/11 17:54:01 by bbrandt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <fcntl.h>
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
